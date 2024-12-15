5a1d6b8445d67346d0aaed33fc79858ab7363eaa
SSovelich <163519655+Ssovelich@users.noreply.github.com>
2024-12-15 19:20:53 +0200

    payload => contactData

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -4,22 +4,27 @@ export const getContacts = () => ContactCollection.find();
 
 export const getContactById = (id) => ContactCollection.findById(id);
 
-export const addContact = (payload) => ContactCollection.create(payload);
+export const addContact = (contactData) =>
+  ContactCollection.create(contactData);
 
 // робимо асинхронний (async), бо тут буде кілька операцій
-export const updateContact = async (_id, payload, options = {}) => {
+export const updateContact = async (_id, contactData, options = {}) => {
   const { upsert = false } = options;
   // Якщо передати 3-м аргументом об'єкт {new: true,}
   // то буде повертатися оновлений об'єкт
-  const result = await ContactCollection.findOneAndUpdate({ _id }, payload, {
-    new: true,
-    // якщо об'єкт не знадено, то він створиться
-    upsert,
-    // додаються в об'єкт відповіді додаткові значення
-    // для визначення чи додався чи оновився об'єк
-    // якщо є поле upserted, то об'єкт було додано
-    includeResultMetadata: true,
-  });
+  const result = await ContactCollection.findOneAndUpdate(
+    { _id },
+    contactData,
+    {
+      new: true,
+      // якщо об'єкт не знадено, то він створиться
+      upsert,
+      // додаються в об'єкт відповіді додаткові значення
+      // для визначення чи додався чи оновився об'єк
+      // якщо є поле upserted, то об'єкт було додано
+      includeResultMetadata: true,
+    },
+  );
 
   if (!result || !result.value) return null;
 
