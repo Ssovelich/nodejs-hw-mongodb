94ca8e705f8e8b3b92eb31beb955dec19425e370
SSovelich <163519655+Ssovelich@users.noreply.github.com>
2024-12-18 13:11:29 +0200

    commit

--- src/middlewares/isValidId.js
+++ src/middlewares/isValidId.js
@@ -2,12 +2,9 @@ import { isValidObjectId } from 'mongoose';
 import createHttpError from 'http-errors';
 
 export const isValidId = (req, res, next) => {
-  const { contactId } = req.params;
-  if (!isValidObjectId(contactId)) {
-    throw createHttpError(
-      400,
-      `The contact ID: ${contactId} is not a valid object`,
-    );
+  const { id } = req.params;
+  if (!isValidObjectId(id)) {
+    throw createHttpError(400, `The contact ID: ${id} is not a valid object`);
   }
 
   next();
